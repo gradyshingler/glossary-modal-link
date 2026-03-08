c60761b634b53fc1d72e8096ed57c329bb6fe564
Grady Shingler <gshingler@dvinci.com>
2026-03-07 23:09:15 -0500

    adding generalizations and removing specific CSS

--- src/Controller/GlossaryModalController.php
+++ src/Controller/GlossaryModalController.php
@@ -11,32 +11,31 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 final class GlossaryModalController extends ControllerBase {
-  private RendererInterface $renderer;
+    private RendererInterface $renderer;
 
-  public function __construct(RendererInterface $renderer) {
-    $this->renderer = $renderer;
-  }
-
-  public static function create(ContainerInterface $container): self {
-    return new static(
-      $container->get('renderer')
-    );
-  }
-
-  public function title(EntityInterface $entity): string {
-    return $entity->label();
-  }
-
-  public function entityViewModeRetrieval(string $entity_type, $entity, string $view_mode = 'default') {
-    $node = Node::load($entity->id());
-    if ($node) {
-      $view_builder = \Drupal::entityTypeManager()->getViewBuilder('node');
-      $content = $view_builder->view($node, $view_mode);
-
-      $rendered_node = \Drupal::service('renderer')->renderRoot($content);
-      $response = new \Symfony\Component\HttpFoundation\Response($rendered_node);
-      return $response;
+    public function __construct(RendererInterface $renderer) {
+        $this->renderer = $renderer;
     }
-    return ['#markup' => $this->t('Content not found.')];
-  }
+
+    public static function create(ContainerInterface $container): self {
+        return new static(
+            $container->get('renderer')
+        );
+    }
+
+    public function title(EntityInterface $entity): string {
+        return $entity->label();
+    }
+
+    public function entityViewModeRetrieval(string $entity_type, EntityInterface $entity, string $view_mode = 'default') {
+        if ($entity) {
+            $view_builder = $this->entityTypeManager()->getViewBuilder($entity_type);
+            $content = $view_builder->view($entity, $view_mode);
+
+            $rendered_content = $this->renderer->renderRoot($content);
+            $response = new Response($rendered_content);
+            return $response;
+        }
+        return ['#markup' => $this->t('Content not found.')];
+    }
 }
